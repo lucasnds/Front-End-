e0e69eab34bd536a4ab877a208697188d92aca6f
Lucas <lucasdoismile16@gmail.com>
2023-01-18 00:49:55 -0300

    adicionei os btn-flags mas ainda nn implantei a funcionalidade, mudei a cor footer

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
+
+// const brazil = document.getElementById('brazil')
+// const usa = document.getElementById('usa')
+// const h1 = document.
+// const h4 = document.
+
+// usa.addEventListener('click', (e) => {
+
+//     e.innerText
+
+// })
