86ea25a856a9c72fab9023f9cddedc0d76f880c5
Lucas <lucasdoismile16@gmail.com>
2023-01-20 03:54:44 -0300

    adicionei svgs para representar o modo light on-off da página, adicionei as flags para a versão mobile

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const chk = document.getElementById('chk')
+// const chk = document.getElementsByClassName('btn-on-off')
+// const image = document.getElementById('image-on-off')
 
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark')
-})
+
+
+// chk.addEventListener("click", () => {
+//     document.body.classList.toggle('dark');
+//     image.classList.remove("fas fa-sun");
+//     image.classList.add("fas fa-moon");
+// })
 
 // const brazil = document.getElementById('brazil')
 // const usa = document.getElementById('usa')
@@ -14,3 +19,45 @@ chk.addEventListener('change', () => {
 //     e.innerText
 
 // })
+//________________________________________________________________
+const btn = document.querySelector('.btn-on-off');
+
+let lua = "./Arquivos/lua.edit.svg"
+let sol = "./Arquivos/sol.edit.svg"
+
+btn.addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+    // Adicionar a mudança de cor dos outros elementos
+    //
+    //
+    //
+    //
+
+    if (document.getElementById("image").src == sol) {
+        document.getElementById("image").src = lua;
+        console.log('lua')
+    } else if (document.getElementById("image").src == lua) {
+        document.getElementById("image").src = sol;
+        console.log('sol')
+    }
+});
+console.log("fim")
+    //_______________________________________________________________
+
+// const btn = document.querySelector('.btn-on-off');
+
+// const lua = "./Arquivos/lua.edit.svg"
+// const sol = "./Arquivos/sol.edit.svg"
+
+// let image = document.getElementById("image");
+
+// btn.addEventListener('click', function() {
+//     document.body.classList.toggle('dark');
+//     if (image.src.endsWith(sol)) {
+//         image.src = lua;
+//         console.log("lua")
+//     } else {
+//         image.src = sol;
+//         console.log("sol")
+//     }
+// });
