144ced79fa8a31bfd8f915351d9efb94184cd67a
Lucas <lucasdoismile16@gmail.com>
2023-01-27 18:38:51 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const h1Title = "Hi, I'm Lucas Nascimento"
 const h4Title = "Front-end developer"
 const pTitle = "I am a software engineering student and appreciator, focused on website and systems development and a dream maker. Always looking to improve my knowledge through projects, courses and exchange of experiences. Focusing in my dream."
 const h3Skills = "My Skills"
-const h3Passions = "Passions"
+const h3Hobbies = "Hobbies"
 const home = "HOME"
 const about = "ABOUT"
 const projects = "PROJECTS"
@@ -15,7 +15,7 @@ const h1TagAbout = document.querySelector('.header-responsive h1')
 const h4TagAbout = document.querySelector('.header-responsive h4')
 const pTagAbout = document.querySelector('.text p')
 const h3TagSkills = document.querySelector('.skills h3')
-const h3TagPassions = document.querySelector('.passions h3')
+const h3TagHobbies = document.querySelector('.passions h3')
 const homeMenuTag = document.querySelector("a[href='#home']");
 const aboutMenuTag = document.querySelector("a[href='#about']");
 const projectsMenuTag = document.querySelector("a[href='#projects']");
@@ -28,7 +28,7 @@ h1TagAbout.textContent = h1Title;
 h4TagAbout.textContent = h4Title;
 pTagAbout.textContent = pTitle;
 h3TagSkills.textContent = h3Skills;
-h3TagPassions.textContent = h3Passions;
+h3TagHobbies.textContent = h3Hobbies;
 homeMenuTag.textContent = home;
 aboutMenuTag.textContent = about;
 projectsMenuTag.textContent = projects;
@@ -44,7 +44,7 @@ brazil.addEventListener('click', function() {
     const h4TitlePTBR = "Desenvolvedor Front-End"
     const pTitlePTBR = "Sou estudante e apreciador da engenharia de software, focado em desenvolvimento de sites e sistemas e um realizador de sonhos. Sempre buscando aprimorar meus conhecimentos através de projetos, cursos e troca de experiências. Focando no meu sonho."
     const h3SkillsPTBR = "Minhas Habilidades"
-    const h3PassionsPTBR = "Paixões"
+    const h3HobbiesPTBR = "Passatempos"
         // const homePTBR = "HOME"
     const aboutPTBR = "SOBRE"
     const projectsPTBR = "PROJETOS"
@@ -57,7 +57,7 @@ brazil.addEventListener('click', function() {
     h4TagAbout.textContent = h4TitlePTBR;
     pTagAbout.textContent = pTitlePTBR;
     h3TagSkills.textContent = h3SkillsPTBR;
-    h3TagPassions.textContent = h3PassionsPTBR;
+    h3TagHobbies.textContent = h3HobbiesPTBR;
     homeMenuTag.textContent = home;
     aboutMenuTag.textContent = aboutPTBR;
     projectsMenuTag.textContent = projectsPTBR;
@@ -76,7 +76,7 @@ usa.addEventListener('click', function() {
     h4TagAbout.textContent = h4Title;
     pTagAbout.textContent = pTitle;
     h3TagSkills.textContent = h3Skills;
-    h3TagPassions.textContent = h3Passions;
+    h3TagHobbies.textContent = h3Hobbies;
     homeMenuTag.textContent = home;
     aboutMenuTag.textContent = about;
     projectsMenuTag.textContent = projects;
@@ -89,8 +89,8 @@ usa.addEventListener('click', function() {
 const btn = document.querySelector('.btn-on-off');
 const h1TagAboutDark = document.querySelector('.text h1')
 const pTagAboutDark = document.querySelector('.text p')
-const h3TagSkillsAndPassionsDark = document.querySelectorAll('#skillsAndPassions h3')
-const ulTagSkillsAndPassionsDark = document.querySelectorAll('ul')
+const h3TagSkillsAndHobbiesDark = document.querySelectorAll('#skillsAndPassions h3')
+const ulTagSkillsAndHobbiesDark = document.querySelectorAll('ul')
 const circle = document.querySelectorAll('.circle-black');
 
 
@@ -106,10 +106,10 @@ btn.addEventListener('click', function() {
     h1TagAboutDark.classList.toggle('dark');
     pTagAboutDark.classList.toggle('dark');
     //h3TagSkillsAndPassionsDark.classList.toggle('dark');
-    h3TagSkillsAndPassionsDark.forEach(h3 => {
+    h3TagSkillsAndHobbiesDark.forEach(h3 => {
         h3.classList.toggle("dark");
     });
-    ulTagSkillsAndPassionsDark.forEach(ul => {
+    ulTagSkillsAndHobbiesDark.forEach(ul => {
         ul.classList.toggle("dark");
     });
 
---
+++ js/download.js
@@ -0,0 +1,8 @@
+// const image = document.querySelector('.download')
+// const downloadcv = document.querySelector('.download-cv span')
+// const downloadcvcapa = document.querySelector('.download-cv.capa span')
+
+// image.addEventListener('mouseover', function() {
+
+
+// })
